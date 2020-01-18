68384f9e4591df3902d69127d87115c4764085d6
dealer426 <burnssamuel01@gmail.com>
2020-01-17 19:54:37 -0500

    Built Cat Class

---
+++ Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sprint2oop
+{
+    public class Cat
+    {
+
+        //Here are the Properties
+        public string Name { get; set; }
+        public string FurColor { get; set; }
+        public int Age { get; set; }
+        public int Lives { get; set; }
+
+
+        //Add the Class Contstructor for Default Object Creation
+        public Cat(string name, string furColor, int age, int lives)
+        {
+            Name = name;
+            FurColor = furColor;
+            Age = age;
+            Lives = lives;
+        }
+
+
+
+
+
+    }
+}
